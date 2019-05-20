d87a78eafbbca724d241f1fc91b6072617513e4b
Kateryna Strorozh <kateryna.storozh@trinetix.com>
2019-05-20 11:33:02 +0300

    Add constants file to project

---
+++ app/src/main/java/com/unitfactory/notesaplication/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.unitfactory.notesaplication.utils;
+
+public interface Constants {
+    int NEW_NOTE_ACTIVITY_REQUEST_CODE = 1;
+    int UPDATED_NOTE_ACTIVITY_REQUEST_CODE = 2;
+    int RESULT_DELETED = 3;
+    String NOTE_EXTRAS_KEY = "note";
+    String EXTRA_REPLY_NOTE = "REPLY";
+    String FULL_QUERY_TEXT = "_";
+    int ASCENDING = 0;
+    int DESCENDING = 1;
+}
